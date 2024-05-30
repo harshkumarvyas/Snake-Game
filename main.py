54498518f7fc952f7792ba96c8871ac92ceceace
Harsh Vyas <harshvyas5527+github@gmail.com>
2024-05-30 11:45:20 +0530

    Ready to build snake game.

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor('black')
+screen.title('My snake game')
+
+starting_positions = [(0, 0),(-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_turtle = Turtle('square')
+    new_turtle.color('white')
+    new_turtle.goto(position)
+
+
+screen.exitonclick()
